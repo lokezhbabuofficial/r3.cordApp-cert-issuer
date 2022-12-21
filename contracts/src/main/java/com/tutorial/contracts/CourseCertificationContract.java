de19727145e960f6a8a1ab8602ddcbb84b845670
Lokesh Babu Mani <lokeshbabuofficial@gmail.com>
2022-12-22 03:45:00 +0530

    Use Case 2: Adding suppot for issuing certificate to bulk students by examinar

--- contracts/src/main/java/com/tutorial/contracts/CourseCertificationContract.java
+++ contracts/src/main/java/com/tutorial/contracts/CourseCertificationContract.java
@@ -11,6 +11,9 @@ import net.corda.core.transactions.LedgerTransaction;
 
 import static net.corda.core.contracts.ContractsDSL.requireThat;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class CourseCertificationContract implements Contract {
 
 	private static final CordaX500Name Examinar = CordaX500Name.parse("O=CordaExaminar,L=Tamilnadu,C=IN");
@@ -24,16 +27,30 @@ public class CourseCertificationContract implements Contract {
 		if (commandData instanceof CourseCertificationContract.Commands.Issue) {
 			CourseCertificationState output = tx.outputsOfType(CourseCertificationState.class).get(0);
 			requireThat(require -> {
-				require.using("This transaction should not have any input",
-						tx.getInputs().isEmpty());
+				require.using("This transaction should not have any input", tx.getInputs().isEmpty());
 				require.using("This transaction should only have one CourseCertificationState state as output",
 						tx.getOutputs().size() == 1);
 				require.using("The score has to be above 80 and less than 100",
 						output.getScore() >= 80 && output.getScore() <= 100);
-				require.using("The student cannot be an examinar",
-						!output.getStudent().getName().equals(Examinar));
-				require.using("Selected examinar is not an actual examinar",
-						output.getExaminer().getName().equals(Examinar));
+				require.using("The student cannot be an examinar", !output.getStudent().getName().equals(Examinar));
+				require.using("Selected examinar is not an actual examinar", output.getExaminer().getName().equals(Examinar));
+				return null;
+			});
+		} else if (commandData instanceof CourseCertificationContract.Commands.BulkIssue) {
+			List<CourseCertificationState> output = tx.outputsOfType(CourseCertificationState.class);
+			requireThat(require -> {
+				require.using("This transaction should not have any input", tx.getInputs().isEmpty());
+
+				require.using("This transaction should be gte 1 CourseCertificationState state as output",
+						tx.getOutputs().size() >= 1);
+
+				require.using("All output should have the actual examinar node",
+						output.stream().map(out -> out.getExaminer().getName()).collect(Collectors.toList()).stream()
+								.allMatch(examinar -> examinar.equals(Examinar)));
+
+				require.using("All output score has to be above 80 and less than 100",
+						output.stream().map(out -> out.getScore()).collect(Collectors.toList()).stream()
+								.allMatch(score -> score >= 80 && score <= 100));
 				return null;
 			});
 		} else {
@@ -43,6 +60,9 @@ public class CourseCertificationContract implements Contract {
 
 	public interface Commands extends CommandData {
 		class Issue implements CourseCertificationContract.Commands {
+		}
+
+		class BulkIssue implements CourseCertificationContract.Commands {
 		}
 	}
 
