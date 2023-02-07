50438697b3c1b9b1dbb7d498c160941fb1e00b7b
Lokesh Babu Mani <lokezhbabuofficial@gmail.com>
2023-02-07 00:32:17 -0500

    Use Case 1 - Added the client controller

---
+++ clients/src/main/java/com/tutorial/web/controller/BaseController.java
@@ -0,0 +1,136 @@
+package com.tutorial.web.controller;
+
+import net.corda.core.messaging.CordaRPCOps;
+import net.corda.core.node.NodeInfo;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.asn1.x500.style.BCStyle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.tutorial.web.response.APIResponse;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
+
+@RestController
+@RequestMapping("/api/base")
+@Api(value = "BaseController", description = "R3.Corda-POC Issue Cert - Base", tags = "Base")
+public class BaseController {
+
+  @Autowired
+  public CordaRPCOps lokeshProxy;
+
+  @Autowired
+  public CordaRPCOps varatharajProxy;
+
+  @Autowired
+  public CordaRPCOps cordaExaminarProxy;
+
+  @Autowired
+  @Qualifier("cordaExaminarProxy")
+  public CordaRPCOps activeUser;
+
+  /** Helpers for filtering the network map cache. */
+  public String toDisplayString(X500Name name) {
+    return BCStyle.INSTANCE.toString(name);
+  }
+
+  public boolean isNotary(NodeInfo nodeInfo) {
+    return !activeUser.notaryIdentities()
+        .stream().filter(el -> nodeInfo.isLegalIdentity(el))
+        .collect(Collectors.toList()).isEmpty();
+  }
+
+  public boolean isMe(NodeInfo nodeInfo) {
+    return nodeInfo.getLegalIdentities().get(0).getName()
+        .equals(activeUser.nodeInfo().getLegalIdentities().get(0).getName());
+  }
+
+  public boolean isNetworkMap(NodeInfo nodeInfo) {
+    return nodeInfo.getLegalIdentities().get(0).getName().getOrganisation().equals("Network Map Service");
+  }
+
+  @GetMapping(value = "/active-user",produces = APPLICATION_JSON_VALUE)
+  @ApiOperation(value = "Active User")
+  private ResponseEntity<HashMap<String, String>> whoami(){
+      HashMap<String, String> myMap = new HashMap<>();
+      myMap.put("me", activeUser.nodeInfo().getLegalIdentities().get(0).getName().toString());
+      return ResponseEntity.status(HttpStatus.OK).body(myMap);
+  }
+
+  @PostMapping(value = "/switch-user/{user}")
+  @ApiOperation(value = "Switch between users (party)")
+  private ResponseEntity<APIResponse<CordaRPCOps>> switchParty(@PathVariable String user) {
+    if (user.equals("Lokesh")) {
+      activeUser = lokeshProxy;
+    } else if (user.equals("Varatharaj")) {
+      activeUser = varatharajProxy;
+    } else if (user.equals("CordaExaminar")) {
+      activeUser = cordaExaminarProxy;
+    } else {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(APIResponse.error("Unrecognised User"));
+    }
+    return ResponseEntity.status(HttpStatus.OK).body(APIResponse.success(activeUser));
+  }
+
+  @GetMapping(value = "/peers", produces = APPLICATION_JSON_VALUE)
+  @ApiOperation(value = "Active user peers")
+  private ResponseEntity<HashMap<String, List<String>>> getPeers() {
+    HashMap<String, List<String>> myMap = new HashMap<>();
+
+    // Find all nodes that are not notaries, ourself, or the network map.
+    Stream<NodeInfo> filteredNodes = activeUser.networkMapSnapshot().stream()
+        .filter(el -> !isNotary(el) && !isMe(el) && !isNetworkMap(el));
+    // Get their names as strings
+    List<String> nodeNames = filteredNodes.map(el -> el.getLegalIdentities().get(0).getName().toString())
+        .collect(Collectors.toList());
+
+    myMap.put("peers", nodeNames);
+    return ResponseEntity.status(HttpStatus.OK).body(myMap);
+  }
+
+  @GetMapping(value = "/flows", produces = APPLICATION_JSON_VALUE)
+  @ApiOperation(value = "Active user flow list")
+  private ResponseEntity<List<String>> flows() {
+    return ResponseEntity.status(HttpStatus.OK).body(activeUser.registeredFlows());
+  }
+
+  @GetMapping(value = "/servertime", produces = TEXT_PLAIN_VALUE)
+    private String serverTime() {
+        return (LocalDateTime.ofInstant(activeUser.currentNodeTime(), ZoneId.of("UTC"))).toString();
+    }
+
+    @GetMapping(value = "/addresses", produces = TEXT_PLAIN_VALUE)
+    private String addresses() {
+        return activeUser.nodeInfo().getAddresses().toString();
+    }
+
+    @GetMapping(value = "/identities", produces = TEXT_PLAIN_VALUE)
+    private String identities() {
+        return activeUser.nodeInfo().getLegalIdentities().toString();
+    }
+
+    @GetMapping(value = "/platformversion", produces = TEXT_PLAIN_VALUE)
+    private String platformVersion() {
+        return Integer.toString(activeUser.nodeInfo().getPlatformVersion());
+    }
+
+    @GetMapping(value = "/notaries", produces = TEXT_PLAIN_VALUE)
+    private String notaries() {
+        return activeUser.notaryIdentities().toString();
+    }
+}
---
+++ clients/src/main/java/com/tutorial/web/request/V1CourseCertificationFlowRequest.java
@@ -0,0 +1,29 @@
+package com.tutorial.web.request;
+
+import io.swagger.annotations.ApiModelProperty;
+
+public class V1CourseCertificationFlowRequest {
+
+  @ApiModelProperty(notes = "Name of the Examinar", name = "party", required = true, value = "O=CordaExaminar,L=Tamilnadu,C=IN")
+  private String party;
+
+  @ApiModelProperty(notes = "Course Score", name = "courseScore", required = true, value = "87")
+  private Integer courseScore;
+
+  public String getParty() {
+    return party;
+  }
+
+  public void setParty(String party) {
+    this.party = party;
+  }
+
+  public Integer getCourseScore() {
+    return courseScore;
+  }
+
+  public void setCourseScore(Integer courseScore) {
+    this.courseScore = courseScore;
+  }
+
+}
